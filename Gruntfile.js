7e41f501eb79befda7b4ee36c5b415ed6f3b4c56
zrh <459676797@qq.com>
2016-09-02 17:19:24 +0800

    样式大调

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,16 +11,19 @@ module.exports = function (grunt) {
                 src: [
                     'js/controller/login.js',
                     'js/controller/top_bar.js',
-                    'js/controller/product.js',
-                    'js/controller/company_message.js',
-                    'js/controller/my_project.js',
-                    'js/controller/super.js',
-                    'js/controller/sign_up.js',
-                    'js/controller/bank.js',
-                    'js/controller/message.js',
+                    'js/controller/super/product.js',
+                    'js/controller/super/manage.js',
+                    'js/controller/super/sign_up.js',
+                    'js/controller/super/bank.js',
+                    'js/controller/admin/company_message.js',
+                    'js/controller/admin/my_project.js',
+                    'js/controller/admin/channel.js',
+                    'js/controller/admin/share.js',
+                    'js/controller/admin/message.js',
                     'js/app.js',
                     'js/route.js'
                 ],
+
                 dest: 'dist/<%= pkg.name %>.js'
             },
             css: {
