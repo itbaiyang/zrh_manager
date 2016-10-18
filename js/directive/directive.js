9aed24e778993f9e3a390855b8dcd09349225533
zrh <459676797@qq.com>
2016-10-18 12:02:47 +0800

    样式大调

--- js/directive/directive.js
+++ js/directive/directive.js
@@ -1,4 +1,4 @@
-app.directive('imageonload', function () {
+detail_app.directive('imageonload', function () {
     return {
         restrict: 'A', link: function (scope, element, attrs) {
             element.bind('load', function () {
